2b7a9b06c3449d761deda8dc7e307de1821b7ef0
ilhamhafidz404 <xxspanzie@gmail.com>
2022-03-24 08:55:57 +0700

    Custom Navigation

--- resources/views/vendor/jetstream/components/nav-link.blade.php
+++ resources/views/vendor/jetstream/components/nav-link.blade.php
@@ -1,28 +1,34 @@
-<a href="{{ route('dashboard') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition  hover:border-gray-300
+<a href="{{ route('dashboard') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition
     @if (request()->is('dashboard'))
         border-indigo-400
+        hover:border-indigo-300
     @else
         border-transparent
+        hover:border-gray-300
     @endif
 ">
     Dashboard
 </a>
 
-<a href="{{ route('index.lesson') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition hover:border-gray-300
+<a href="{{ route('index.lesson') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition
     @if (request()->is('lessons*'))
         border-indigo-400
+        hover:border-indigo-300
     @else
         border-transparent
+        hover:border-gray-300
     @endif
 ">
     Pelajaran
 </a>
 
-<a href="{{ route('add.question') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition hover:border-gray-300
+<a href="{{ route('add.question') }}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition
     @if (request()->is('addquestion*'))
         border-indigo-400
+        hover:border-indigo-300
     @else
         border-transparent
+        hover:border-gray-300
     @endif
 ">
     Buat Soal
