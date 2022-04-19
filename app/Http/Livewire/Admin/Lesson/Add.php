1f91180a533c50e21bee48e22683ef90a7f555ea
ilhamhafidz404 <xxspanzie@gmail.com>
2022-04-19 23:27:39 +0700

    Add Mapel in admin page

--- app/Http/Livewire/Admin/Lesson/Add.php
+++ app/Http/Livewire/Admin/Lesson/Add.php
@@ -3,14 +3,37 @@
 namespace App\Http\Livewire\Admin\Lesson;
 
 use Livewire\Component;
-use App\Models\Lesson;
+use App\Models\{ Lesson, User};
+use Illuminate\Support\Str;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Add extends Component
 {
+
+    use LivewireAlert;
+
     public function render()
     {
         return view('livewire.admin.lesson.add', [
-            'lessons' => Lesson::whereUserId(null)->get()
+            'teachers' => User::role('teacher')->get()
         ])->extends('livewire.admin.master')->section('content');
+    }
+
+    public $name;
+    protected $rules = [
+        'name'      => 'required',
+    ];
+    public function store(){
+        $this->validate();
+
+        Lesson::create([
+            'name'      => $this->name,
+            'slug'      => Str::slug($this->name),
+            'status'    => 'not'
+        ]);
+
+        $this->name = null;
+
+        $this->alert('success', 'Berhasil Menambah Mapel');
     }
 }
--- resources/views/livewire/Admin/lesson/add.blade.php
+++ resources/views/livewire/Admin/lesson/add.blade.php
@@ -4,40 +4,42 @@
     @endsection
 
     <div class="card p-3">
-        <form action="">
+        <form wire:submit.prevent="store()">
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="">Nama :</label>
-                        <input type="text" class="form-control">
+                        <label for="name">Nama :</label>
+                        <input 
+                            type="text" 
+                            class="
+                                form-control
+                                @error('name')
+                                    is-invalid
+                                @enderror
+                            "
+                            wire:model="name"
+                            id="name"
+                            :value="old('name')"
+                        >
+                        @error('name')
+                            <i class="invalid-feedback">{{ $message }}</i>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label for="">Email :</label>
-                        <input type="email" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="">Password :</label>
-                        <input type="password" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="">Confirm Password :</label>
-                        <input type="password" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="">Select Lesson :</label>
+                        <label for="">Pilih Guru Pengajar :</label>
                         <select name="" id="" class="form-control">
-                            @foreach ($lessons as $lesson)
-                                <option value="">{{ $lesson->name }}</option>
+                            @foreach ($teachers as $teacher)
+                                <option value="">{{ $teacher->name }}</option>
                             @endforeach
                         </select>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="d-flex align-items-center justify-content-end">
+                        <button class="btn btn-danger mr-3" type="reset">Reset</button>
+                        <button class="btn btn-primary" type="submit">Submit</button>
                     </div>
                 </div>
             </div>
